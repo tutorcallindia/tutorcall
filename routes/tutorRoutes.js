9f4552efbea41516c533c11ed28e9ab93ad7de61
tutorcallindia <suresmartkidz@gmail.com>
2026-05-31 02:21:11 +0530

    fix booking model case

--- routes/tutorRoutes.js
+++ routes/tutorRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const Booking = require("../models/Booking");
+const Booking = require("../models/booking");
 
 const jwt = require("jsonwebtoken");
 
