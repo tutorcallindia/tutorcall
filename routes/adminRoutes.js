5adc7b3f2ccaece477e225760130eb9a5d5cf8e6
tutorcallindia <suresmartkidz@gmail.com>
2026-05-31 03:09:47 +0530

    fix admin tutor import

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const Tutor = require("../models/Tutor");
+const Tutor = require("../models/tutor");
 /* =========================================
               ADMIN LOGIN
 ========================================= */
