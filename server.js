6192a6b9b8ac0fc1ed26173c6f18cdc84dd47d1b
tutorcallindia <suresmartkidz@gmail.com>
2026-06-05 12:47:29 +0530

    serve html files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ console.log("MONGO URI =", process.env.MONGO_URI ? "FOUND" : "NOT FOUND");
 
 const express =
   require("express");
-  const path = require("path");
+  
 
 const mongoose =
   require("mongoose");
@@ -26,7 +26,7 @@ const app =
 app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
-app.use(express.static(path.join(__dirname)));
+
 
 app.use(express.urlencoded({
   limit: "50mb",
