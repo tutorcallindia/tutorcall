37fabfcb65dbf0ee61bff55b3582a8077192dcd2
tutorcallindia <suresmartkidz@gmail.com>
2026-09-19 22:37:30 +0530

    Use Firebase credentials from environment variables

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -2,9 +2,33 @@ const { initializeApp, cert, getApps } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 const path = require("path");
 
-const serviceAccount = require(
-  path.join(__dirname, "..", "firebase-service-account.json")
-);
+let serviceAccount;
+
+if (
+  process.env.FIREBASE_PROJECT_ID &&
+  process.env.FIREBASE_PRIVATE_KEY &&
+  process.env.FIREBASE_CLIENT_EMAIL
+) {
+  serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url:
+      process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+  };
+} else {
+  serviceAccount = require(
+    path.join(__dirname, "..", "firebase-service-account.json")
+  );
+}
 
 const app =
   getApps().length === 0
